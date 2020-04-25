5429e1fa4d2f22262c448e5e67e8df8ad5965fb3
Daniel Min <danielm710@neufeldserver.uwaterloo.ca>
2020-04-25 19:10:05 -0400

    export qiime2 file helper added

--- scripts/qiime2_helper/export_qiime_artifact.py
+++ scripts/qiime2_helper/export_qiime_artifact.py
@@ -1,7 +1,11 @@
 from qiime2 import Artifact
 from argparse import ArgumentParser
 import pandas as pd
+from skbio.stats import ordination
 import sys
+import logging
+
+logger = logging.getLogger(__name__)
 
 def args_parse():
     """
@@ -22,14 +26,45 @@ def args_parse():
 
     return parser
 
-def convert(artifact_path, output_path):
+def convert(artifact_path):
+    """
+    Converts QIIME2 artifact to tsv if applicabl if applicable
+
+    Input:
+        - artifact_path: path to QIIME2 artifact (.qza)
+        - output_path: path to save output as
+    Returns:
+        - Dictionary with pandas series or dataframe as values
+    """
     artifact = Artifact.load(artifact_path)
     artifact_type = str(artifact.type)
 
-    if(artifact_type == "FeatureTable[Frequency]"):
+    if(artifact_type == "FeatureTable[Frequency]" or
+        artifact_type == "FeatureTable[RelativeFrequency]"):
         df = artifact.view(pd.DataFrame)
 
-        df.to_csv(output_path, sep='\t')
+        output = {
+                "feature_table": df
+                }
+
+        return output
+    elif(artifact_type == "PCoAResults"):
+        ordination_result = artifact.view(ordination.OrdinationResults)
+
+        eigvals = ordination_result.eigvals # pd.DataFrame
+        coordinates = ordination_result.samples # pd.Series
+        proportion_explained = ordination_results.proportion_explained # pd.Series 
+
+        output = {
+                "eigvals": eigvals,
+                "coordinates": coordinates,
+                "proportion_explained": proportion_explained
+                }
+
+        return output
+    else:
+        logger.warning("Could not convert specified QIIME2 artifact.")
+        return {}
 
 if __name__ == '__main__':
     parser = args_parse()
