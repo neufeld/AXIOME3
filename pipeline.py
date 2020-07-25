2ee76e4e2bb92752569f19e953551dd0ca9cb742
Daniel Min <danielm710@gmail.com>
2020-07-25 00:17:27 -0400

    change from observed-features to observed-otus in core metrics phylogenetic
    Former-commit-id: 80590798d6167e3aa0992112306212e2aa28426d

--- pipeline.py
+++ pipeline.py
@@ -1354,7 +1354,7 @@ class Core_Metrics_Phylogeny(luigi.Task):
                 self.output()['rarefied_table'].path,
                 '--o-faith-pd-vector',
                 self.output()['faith_pd_vector'].path,
-                '--o-observed-features-vector',
+                '--o-observed-otus-vector',
                 self.output()['obs_otu_vector'].path,
                 '--o-shannon-vector',
                 self.output()['shannon_vector'].path,
